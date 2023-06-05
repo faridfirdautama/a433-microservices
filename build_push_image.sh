e553e523d70e046e684d4817843353f70d627487
Farid Firda Utama <dadit.utama@gmail.com>
2023-06-05 17:08:51 +0700

    revised command shell

--- build_push_image.sh
+++ build_push_image.sh
@@ -9,11 +9,10 @@ docker build -t item-app:v1 .
 docker images
 
 # to rename docker image
-docker tag item-app:v1 item-app:1.0.0
+docker tag item-app:v1 ghcr.io/faridfirdautama/item-app:1.0.0
 
 # login for ghcr.io & export the password into CR_PAT env variable
 echo $CR_PAT | docker login ghcr.io --username faridfirdautama --password-stdin
 
 # push to Github Packages
-#docker tag item-app ghcr.io/faridfirdautama/item-app:1.0.0
 docker push ghcr.io/faridfirdautama/item-app:1.0.0
